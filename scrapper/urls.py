2274f80c0a819949740839107ba5641e4c9ed401
ajayvahan <ajay.vahan@mindfiresolutions.com>
2016-03-04 12:39:21 +0530

    made changes to urls.py

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -1,10 +1,11 @@
+"""URLs for scrapper project."""
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
 # from django.views.generic import TemplateView
 
 urlpatterns = [
-    # url(r'^templates$', TemplateView.as_view(template_name="hello.html"), name='whatever'),
     url(r'^$', 'apps.home.views.home', name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^signup/$', 'apps.usermanager.views.signup', name='signup'),
@@ -12,9 +13,11 @@ urlpatterns = [
     url(r'^logout$', 'django.contrib.auth.views.logout',
         {"next_page": "/"}, name='logout'),
     url(r'^dashboard/$', 'apps.home.views.dashboard', name='dashboard'),
-    url(r'^dashboard/search/$', 'apps.home.views.dashboard_search', name='dashboard_search'),
+    url(r'^dashboard/search/$', 'apps.home.views.dashboard_search',
+        name='dashboard_search'),
     url(r'^profile/$', 'apps.home.views.profile', name='profile'),
-    url(r'^profile/edit/$', 'apps.home.views.edit_profile', name='edit_profile'),
+    url(r'^profile/edit/$', 'apps.home.views.edit_profile',
+        name='edit_profile'),
     url(r'^dashboard/scrap/$', 'apps.home.views.scrap', name='scrap'),
     url(r'^activate/$', 'apps.usermanager.views.activate', name='activate'),
 
